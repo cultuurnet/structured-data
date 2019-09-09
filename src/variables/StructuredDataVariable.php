adc493b9344b7bd22f1829e75020c3fd538ea680
Jan Henckens <jan@statik.be>
2019-09-09 15:42:28 +0200

    Added caching options for detail page requests

--- src/variables/StructuredDataVariable.php
+++ src/variables/StructuredDataVariable.php
@@ -2,7 +2,9 @@
 
 namespace publiq\structuredData\variables;
 
+use Craft;
 use publiq\structuredData\Plugin;
+use yii\caching\TagDependency;
 
 class StructuredDataVariable
 {
@@ -13,12 +15,25 @@ class StructuredDataVariable
 
     public function getOfferData($cdbid = null, $includePastEvents = false)
     {
-        if(!$cdbid) {
+        if (!$cdbid) {
             $cdbid = substr($_GET['p'], -36);
         }
 
         if (isset($cdbid)) {
-            return Plugin::getInstance()->searchApi->getOfferFromApi($cdbid, $includePastEvents);
+            if (getenv('ENVIRONMENT') == 'dev') {
+                return Plugin::getInstance()->searchApi->getOfferFromApi($cdbid, $includePastEvents);
+            } else {
+                $tags = new TagDependency([
+                    "tags" => [
+                        Plugin::UDB_EVENTS_ALL,
+                        Plugin::UDB_EVENTS_DETAIL,
+                        "udb_events_$cdbid"
+                    ]
+                ]);
+                return Craft::$app->getCache()->getOrSet("udb_events_$cdbid", function () use ($cdbid, $includePastEvents) {
+                    return Plugin::getInstance()->searchApi->getOfferFromApi($cdbid, $includePastEvents);
+                }, 86400, $tags);
+            }
         }
     }
 }
